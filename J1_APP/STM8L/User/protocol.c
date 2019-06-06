bcce87c5d4322bb1da11a6c252160525f33ac7be
e <361107133@qq.com>
2019-06-06 23:07:29 +0800

    20190607: 应用层通信协议完成

--- J1_APP/STM8L/User/protocol.c
+++ J1_APP/STM8L/User/protocol.c
@@ -7,285 +7,41 @@
 #include "timetick.h"
 
 void protocol_reply(void* ptr);
+void protocol_repeat(void* ptr);
+void protocol_command(void* ptr);
 
-TIMER rep_timer[4];
-TIMER cmd_timer[4];
+
+TIMER Rep_Timer[4];
+TIMER Rtp_Timer[4];
 
 PROTOCOL protocol[]= {
-	/*id, 	cmd, 		rep_flag,			rep_ways		rep_cout	rep_nums	rep_time	rep_func 			rep_timer_cb 		cmd_timer_cb */
-	{0, 	APP_CMD_1, 	REP_FLAG_ZERO, 		REP_WYAS_NOW, 	0, 			3, 			1000, 		protocol_reply, 	&rep_timer[0],		&cmd_timer[0]},
-	{1, 	APP_CMD_2, 	REP_FLAG_ZERO, 		REP_WYAS_NOW, 	0, 			3, 			1000, 		protocol_reply, 	&rep_timer[1],		&cmd_timer[1]},
-	{2, 	APP_CMD_3, 	REP_FLAG_ZERO, 		REP_WYAS_NOW, 	0, 			3, 			1000, 		protocol_reply,		&rep_timer[2],		&cmd_timer[2]},
-	{3, 	APP_CMD_4, 	REP_FLAG_ZERO, 		REP_WYAS_NOW, 	0, 			3, 			1000, 		protocol_reply, 	&rep_timer[3],		&cmd_timer[3]}
+	/*id, 	cmd, 		rep_flag,				rep_ways			rep_cout		rep_nums		rep_time		rpt_flag	rep_timer 			rpt_timer */
+	{0, 	APP_CMD_1, 	REP_FLAG_ZERO, 		REP_WYAS_NOW, 	0, 			3, 			1000, 		0,		&Rep_Timer[0],		&Rtp_Timer[0]},
+	{1, 	APP_CMD_2, 	REP_FLAG_ZERO, 		REP_WYAS_NOW, 	0, 			3, 			1000, 		0,		&Rep_Timer[1],		&Rtp_Timer[1]},
+	{2, 	APP_CMD_3, 	REP_FLAG_ZERO, 		REP_WYAS_NOW, 	0, 			3, 			1000, 		0,		&Rep_Timer[2],		&Rtp_Timer[2]},
+	{3, 	APP_CMD_4, 	REP_FLAG_ZERO, 		REP_WYAS_NOW, 	0, 			3, 			1000, 		0,		&Rep_Timer[3],		&Rtp_Timer[3]}
 };
 
 void protocol_reply(void* ptr)
 {
 	PROTOCOL* Pptr = (PROTOCOL*)ptr;
-	
-	uint16_t id 		= Pptr->id;
-	uint16_t cmd       	= Pptr->cmd;
-	uint16_t rep_flag 	= Pptr->rep_flag;
-	uint16_t rep_ways 	= Pptr->rep_ways;
-	uint16_t rep_cout 	= Pptr->rep_cout;
-	uint16_t rep_nums 	= Pptr->rep_nums;
-	uint16_t rep_time 	= Pptr->rep_time;
-	TIMER_CB_FUNC rep_func = Pptr->rep_func; 
-	TIMER*	rep_timer_cb    = Pptr->rep_timer_cb;
-	
+
 	switch(rep_flag)
 	{
-		case REP_FLAG_ZERO:	//应答超时，未在规定时间内收到对方，数据很可能丢失
-		{
-			//调用重传处理
-			#if 0
-			switch(rep_ways)
-			{
-				case REP_WYAS_OVER:	//通信结束，放弃重传
-				{
-					//Do nothing
-					break;
-				}
-				case REP_WYAS_NOW:	//通信继续，立即重传
-				{
-					/* 重传计数不大于最大重传次数 */
-					if(++rep_cout <= rep_nums)
-					{
-						//立即执行重传
-					}
-					break;
-				}
-				case REP_WYAS_INC:	//通信继续，延时rep_cout*1秒重传
-				{
-					if(rep_cout != 0)
-					{
-						//执行重传
-						//send(cmd);
-					}
-					
-					/* 重传计数不大于最大重传次数 */
-					if(++rep_cout <= rep_nums)
-					{
-						//定时(rep_cout*1)秒执行重传
-						timer_cb->timer_name 	= (void *)timer_cb;
-						timer_cb->cb_func	 	= rep_func;
-						timer_cb->cb_data		= &protocol[id];
-						timer_cb->cb_ticks		= SS_TO_TICKS(1)*rep_cout;
-						TimerInsert(timer_cb);
-					}					
-					break;
-				}
-				case REP_WYAS_EXP:	//通信继续，延时2^rep_cout*1秒重传
-				{
-					if(rep_cout != 0)
-					{
-						//执行重传
-						//send(cmd);
-					}
-					
-					/* 重传计数不大于最大重传次数 */
-					if(++rep_cout <= rep_nums)
-					{
-						//定时(2^rep_cout*1)秒执行重传
-						timer_cb->timer_name 	= (void *)timer_cb;
-						timer_cb->cb_func	 	= rep_func;
-						timer_cb->cb_data		= &protocol[id];
-						timer_cb->cb_ticks		= SS_TO_TICKS(1)*(2^rep_cout);
-						TimerInsert(timer_cb);
-					}					
-					break;
-				}
-				case REP_WYAS_DELAY1:	//通信继续，延时1秒重传
-				{
-					if(rep_cout != 0)
-					{
-						//执行重传
-						//send(cmd);
-					}
-					
-					/* 重传计数不大于最大重传次数 */
-					if(++rep_cout <= rep_nums)
-					{
-						//定时(1)秒执行重传
-						timer_cb->timer_name 	= (void *)timer_cb;
-						timer_cb->cb_func	 	= rep_func;
-						timer_cb->cb_data		= &protocol[id];
-						timer_cb->cb_ticks		= SS_TO_TICKS(1);
-						TimerInsert(timer_cb);
-					}					
-					break;
-				}
-				case REP_WYAS_DELAY2:	//通信继续，延时2秒重传
-				{
-					if(rep_cout != 0)
-					{
-						//执行重传
-						//send(cmd);
-					}
-					
-					/* 重传计数不大于最大重传次数 */
-					if(++rep_cout <= rep_nums)
-					{
-						//定时(2)秒执行重传
-						timer_cb->timer_name 	= (void *)timer_cb;
-						timer_cb->cb_func	 	= rep_func;
-						timer_cb->cb_data		= &protocol[id];
-						timer_cb->cb_ticks		= SS_TO_TICKS(2);
-						TimerInsert(timer_cb);
-					}					
-					break;
-				}
-				case REP_WYAS_DELAY3:	//通信继续，延时3秒重传
-				{
-					if(rep_cout != 0)
-					{
-						//执行重传
-						//send(cmd);
-					}
-					
-					/* 重传计数不大于最大重传次数 */
-					if(++rep_cout <= rep_nums)
-					{
-						//定时(3)秒执行重传
-						timer_cb->timer_name 	= (void *)timer_cb;
-						timer_cb->cb_func	 	= rep_func;
-						timer_cb->cb_data		= &protocol[id];
-						timer_cb->cb_ticks		= SS_TO_TICKS(3);
-						TimerInsert(timer_cb);
-					}					
-					break;
-				}
-				case REP_WYAS_DELAY5:	//通信继续，延时5秒重传
-				{
-					if(rep_cout != 0)
-					{
-						//执行重传
-						//send(cmd);
-					}
-					
-					/* 重传计数不大于最大重传次数 */
-					if(++rep_cout <= rep_nums)
-					{
-						//定时(5)秒执行重传
-						timer_cb->timer_name 	= (void *)timer_cb;
-						timer_cb->cb_func	 	= rep_func;
-						timer_cb->cb_data		= &protocol[id];
-						timer_cb->cb_ticks		= SS_TO_TICKS(5);
-						TimerInsert(timer_cb);
-					}					
-					break;
-				}
-				case REP_WYAS_DELAY7:	//通信继续，延时7秒重传
-				{
-					if(rep_cout != 0)
-					{
-						//执行重传
-						//send(cmd);
-					}
-					
-					/* 重传计数不大于最大重传次数 */
-					if(++rep_cout <= rep_nums)
-					{
-						//定时(7)秒执行重传
-						timer_cb->timer_name 	= (void *)timer_cb;
-						timer_cb->cb_func	 	= rep_func;
-						timer_cb->cb_data		= &protocol[id];
-						timer_cb->cb_ticks		= SS_TO_TICKS(7);
-						TimerInsert(timer_cb);
-					}					
-					break;
-				}
-				case REP_WYAS_DELAY10:	//通信继续，延时10秒重传
-				{
-					if(rep_cout != 0)
-					{
-						//执行重传
-						//send(cmd);
-					}
-					
-					/* 重传计数不大于最大重传次数 */
-					if(++rep_cout <= rep_nums)
-					{
-						//定时(10)秒执行重传
-						timer_cb->timer_name 	= (void *)timer_cb;
-						timer_cb->cb_func	 	= rep_func;
-						timer_cb->cb_data		= &protocol[id];
-						timer_cb->cb_ticks		= SS_TO_TICKS(10);
-						TimerInsert(timer_cb);
-					}					
-					break;
-				}
-				case REP_WYAS_DELAY15:	//通信继续，延时15秒重传
-				{
-					if(rep_cout != 0)
-					{
-						//执行重传
-						//send(cmd);
-					}
-					
-					/* 重传计数不大于最大重传次数 */
-					if(++rep_cout <= rep_nums)
-					{
-						//定时(15)秒执行重传
-						timer_cb->timer_name 	= (void *)timer_cb;
-						timer_cb->cb_func	 	= rep_func;
-						timer_cb->cb_data		= &protocol[id];
-						timer_cb->cb_ticks		= SS_TO_TICKS(15);
-						TimerInsert(timer_cb);
-					}					
-					break;
-				}
-				case REP_WYAS_DELAY30:	//通信继续，延时30秒重传
-				{
-					if(rep_cout != 0)
-					{
-						//执行重传
-						//send(cmd);
-					}
-					
-					/* 重传计数不大于最大重传次数 */
-					if(++rep_cout <= rep_nums)
-					{
-						//定时(30)秒执行重传
-						timer_cb->timer_name 	= (void *)timer_cb;
-						timer_cb->cb_func	 	= rep_func;
-						timer_cb->cb_data		= &protocol[id];
-						timer_cb->cb_ticks		= SS_TO_TICKS(30);
-						TimerInsert(timer_cb);
-					}					
-					break;
-				}
-
-				default:
-					break;
-			}
-			break;
-
-			#endif
-		}
-		case REP_FLAG_ACK:	//应答确认
-		{
-			//一次通信结束
-			break;
-		}
-		case REP_FLAG_SIZE:	//应答数据大小出错
-		{
-			//调用重传处理
-			break;
-		}
+		case REP_FLAG_ACK:	//应答确认通讯无误
 		case REP_FLAG_CMD:	//应答不能识别命令
 		{
 			//一次通信结束
+			//do nothing
 			break;
 		}
+		case REP_FLAG_ZERO:	//应答超时，未在应答超时时间内收到对方，数据很可能丢失
+		case REP_FLAG_SIZE:	//应答数据大小出错
 		case REP_FLAG_CRC:	//应答数据校验错误
-		{
-			//调用重传处理
-			break;
-		}
 		case REP_FLAG_PRMA:	//应答参数错误
 		{
 			//调用重传处理
+			protocol_repeat(Pptr);
 			break;
 		}			
 		default:
@@ -297,6 +53,159 @@ void protocol_reply(void* ptr)
 
 void protocol_repeat(void* ptr)
 {
+	PROTOCOL* Pptr = (PROTOCOL*)ptr;
 	
+	uint16_t rep_ways 	= Pptr->rep_ways;
+	uint16_t rep_cout 	= Pptr->rep_cout;
+	uint16_t rep_nums 	= Pptr->rep_nums;	
+	uint16_t rpt_flag	= Pptr->rpt_flag;
+	TIMER*	rpt_timer   = Pptr->rpt_timer;
+
+	switch(rep_ways)
+	{
+		case REP_WYAS_OVER:	//通信结束，放弃重传
+		{
+			//Do nothing
+			break;
+		}
+		case REP_WYAS_NOW:	//通信继续，立即重传
+		{
+			/* 重传计数不大于最大重传次数 */
+			if(++rep_cout <= rep_nums)
+			{
+				//立即执行重传
+				protocol_command(Pptr);
+			}
+			break;
+		}
+		case REP_WYAS_INC:	//通信继续，延时rep_cout*1秒重传
+		{
+			if(rpt_flag == 0)
+			{				
+				/* 重传计数不大于最大重传次数 */
+				if(++rep_cout <= rep_nums)
+				{
+					Pptr->rpt_flag = 1;
+					
+					//定时(rep_cout*1)秒执行重传
+					rpt_timer->timer_name 	= (void *)rpt_timer;
+					rpt_timer->cb_func	 	= protocol_repeat;
+					rpt_timer->cb_data		= Pptr;
+					rpt_timer->cb_ticks		= SS_TO_TICKS(1)*rep_cout;
+					TimerInsert(rpt_timer);
+				}
+			}
+			else if(rpt_flag == 1)
+			{
+				Pptr->rpt_flag = 0;
+				//执行重传
+				protocol_command(Pptr);	
+			}
+			
+			break;
+		}
+		case REP_WYAS_EXP:	//通信继续，延时2^rep_cout*1秒重传
+		{
+			if(rpt_flag == 0)
+			{				
+				/* 重传计数不大于最大重传次数 */
+				if(++rep_cout <= rep_nums)
+				{
+					Pptr->rpt_flag = 1;
+					
+					//定时(rep_cout*1)秒执行重传
+					rpt_timer->timer_name 	= (void *)rpt_timer;
+					rpt_timer->cb_func	 	= protocol_repeat;
+					rpt_timer->cb_data		= Pptr;
+					rpt_timer->cb_ticks		= SS_TO_TICKS(1)*(2^rep_cout);
+					TimerInsert(rpt_timer);
+				}
+			}
+			else if(rpt_flag == 1)
+			{
+				Pptr->rpt_flag = 0;
+				//执行重传
+				protocol_command(Pptr);	
+			}
+
+			break;
+		}
+		case REP_WYAS_DELAY1:	//通信继续，延时1秒重传
+		{
+			if(rpt_flag == 0)
+			{				
+				/* 重传计数不大于最大重传次数 */
+				if(++rep_cout <= rep_nums)
+				{
+					Pptr->rpt_flag = 1;
+					
+					//定时(rep_cout*1)秒执行重传
+					rpt_timer->timer_name 	= (void *)rpt_timer;
+					rpt_timer->cb_func	 	= protocol_repeat;
+					rpt_timer->cb_data		= Pptr;
+					rpt_timer->cb_ticks		= SS_TO_TICKS(1);
+					TimerInsert(rpt_timer);
+				}
+			}
+			else if(rpt_flag == 1)
+			{
+				Pptr->rpt_flag = 0;
+				//执行重传
+				protocol_command(Pptr);	
+			}
+
+			break;
+		}
+		default:
+			break;
+	}
+			
 }
 
+void protocol_command(void* ptr)
+{
+	PROTOCOL* Pptr = (PROTOCOL*)ptr;
+	
+	uint16_t id 	= Pptr->id;
+	uint16_t cmd 	= Pptr->cmd;
+	uint16_t rep_time 	= Pptr->rep_time;
+	TIMER*	rep_timer  	= Pptr->rep_timer;
+	
+	switch(cmd)
+	{
+		case APP_CMD_1:	//命令1
+		{
+			/* 发送数据 */
+			//send_data(APP_CMD_1);			
+			break;
+		}
+		case APP_CMD_2:	//命令2
+		{
+			/* 发送数据 */
+			//send_data(APP_CMD_2);
+			break;
+		}
+		case APP_CMD_3:	//命令3
+		{
+			/* 发送数据 */
+			//send_data(APP_CMD_3);			
+			break;
+		}
+		case APP_CMD_4:	//命令4
+		{
+			/* 发送数据 */
+			//send_data(APP_CMD_4);
+			break;
+		}
+		default:
+			break;
+	}
+
+	/* 启动应答定时器 */
+	rep_timer->timer_name	= (void*)rep_timer;
+	rep_timer->cb_func		= protocol_reply;
+	rep_timer->cb_data		= Pptr;
+	rep_timer->cb_ticks 	= MS_TO_TICKS(rep_time);
+	TimerInsert(rep_timer);
+	
+}
--- J1_APP/STM8L/User/protocol.h
+++ J1_APP/STM8L/User/protocol.h
@@ -37,14 +37,6 @@ typedef enum
 	REP_WYAS_INC    = ((uint16_t)0x0002),		//协议通信线性递增重传，延时rep_cout秒后重传
 	REP_WYAS_EXP    = ((uint16_t)0x0003),		//协议通信指数递增重传，延时2^rep_cout秒后重传
 	REP_WYAS_DELAY1	= ((uint16_t)0x0004),		//协议通信立延时1秒重传
-	REP_WYAS_DELAY2	= ((uint16_t)0x0005),		//协议通信立延时2秒重传
-	REP_WYAS_DELAY3	= ((uint16_t)0x0006),		//协议通信立延时3秒重传
-	REP_WYAS_DELAY5	= ((uint16_t)0x0007),		//协议通信立延时5秒重传
-	REP_WYAS_DELAY7	= ((uint16_t)0x0008),		//协议通信立延时7秒重传
-	REP_WYAS_DELAY10	= ((uint16_t)0x0009),	//协议通信立延时10秒重传
-	REP_WYAS_DELAY15	= ((uint16_t)0x000A),	//协议通信立延时15秒重传
-	REP_WYAS_DELAY30	= ((uint16_t)0x000B),	//协议通信立延时30秒重传
-	
 }REP_WAYS_TypeDef;
 
 
@@ -57,9 +49,9 @@ typedef struct protocol
 	uint16_t			rep_cout;		//重传计数，初值是0.每重传1次，增加1
 	uint16_t			rep_nums;		//重传次数，设置最大重传次数，重传计数到达最大重传次数，自动放弃重传
 	uint16_t			rep_time;		//应答时间，定时rep_time后，检测命令回复结果，单位ms
-	TIMER_CB_FUNC   	rep_func;    	//应答函数，定时rep_time，对命令回复结果处理函数
-	TIMER*				rep_timer_cb;	//应答定时回调
-	TIMER*				cmd_timer_cb;	//命令重发定时回调
+	uint16_t			rpt_flag;		//命令重发使能标记
+	TIMER*				rep_timer;		//应答定时回调
+	TIMER*				rpt_timer;		//命令重发定时回调
 
 } PROTOCOL;
 
