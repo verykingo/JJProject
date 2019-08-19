199f6e23507da815cfa339ca847ecbe07f95e8f6
wenchengwei@intercel.com.cn <wenchengwei@intercel.com.cn>
2019-08-19 16:58:22 +0800

    添加IAR工程项目文件

--- J1_APP/STM8L/Project/IAR/settings/J1_APP_NoRTOS.Debug.cspy.bat
+++ J1_APP/STM8L/Project/IAR/settings/J1_APP_NoRTOS.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0_2\common\bin\cspybat" -f "E:\Wen\MyPorject\JJProject\J1_APP\STM8L\Project\IAR\settings\J1_APP_NoRTOS.Debug.general.xcl" --backend -f "E:\Wen\MyPorject\JJProject\J1_APP\STM8L\Project\IAR\settings\J1_APP_NoRTOS.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\Wen\MyPorject\JJProject\J1_APP\STM8L\Project\IAR\settings\J1_APP_NoRTOS.Debug.general.xcl" --backend -f "E:\Wen\MyPorject\JJProject\J1_APP\STM8L\Project\IAR\settings\J1_APP_NoRTOS.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0_2\common\bin\cspybat" -f "E:\Wen\MyPorject\JJProject\J1_APP\STM8L\Project\IAR\settings\J1_APP_NoRTOS.Debug.general.xcl" "--debug_file=%~1" --backend -f "E:\Wen\MyPorject\JJProject\J1_APP\STM8L\Project\IAR\settings\J1_APP_NoRTOS.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\Wen\MyPorject\JJProject\J1_APP\STM8L\Project\IAR\settings\J1_APP_NoRTOS.Debug.general.xcl" "--debug_file=%~1" --backend -f "E:\Wen\MyPorject\JJProject\J1_APP\STM8L\Project\IAR\settings\J1_APP_NoRTOS.Debug.driver.xcl" 
 
 @echo off 
 :end
